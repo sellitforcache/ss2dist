3288f1a2e1173dac01084a41a416b8aca2161528
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2018-09-05 09:43:27 +0200

    starting a new, less messy build system

---
+++ examples/test.i
@@ -0,0 +1,26 @@
+test
+1  0 -99 imp:n=1
+99 0  99 imp:n=0
+
+99 so 100
+
+sdef   pos=d1
+       par=n
+       erg=1
+       vec=0 0 1
+       dir=1
+c
+c
+
+c
+c
+c
+c
+c
+nps 1e3
+TMESH
+ rmesh101:n
+  cora101  -5   9i    5
+  corb101  -5   9i    5
+  corc101   1         2
+ENDMD
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup, Extension
 import os
 import numpy as np
 
-ss2py_module = Extension('ss2py', sources=['ss2py/ss2py.cpp','SurfaceSource.cpp','helpers.cpp'])
-
+ss2py_module = Extension('ss2py', sources=['ss2py/ss2py.cpp','src/SurfaceSource.cpp','src/helpers.cpp'])
+print np.get_include(),os.getcwd()+'/src'
 setup(name='ss2py',
       version='1.0',
       description='Module for loading a MCNP wssa surface source file en masse.',
-      include_dirs = [np.get_include(),os.getcwd()],
+      include_dirs = [np.get_include(),os.getcwd()+'/src'],
       ext_modules=[ss2py_module])
